142b049004863e4ff3e44cede5623ce973de9a3a
gonzoMD <gonzomdx@gmail.com>
2022-03-06 23:39:14 +0100

    [ICONTHEMECONVERTER] Add a Python Version
    This is WIP.
    currently only 32bit color icons with optional canvas size supported
    
    TODO: Add missing stuff: Overlays, lower color depths, bitmaps (toolbars
    etc.)
    TODO: cleanup temp files

---
+++ IconThemeConverter.py
@@ -0,0 +1,57 @@
+import os
+import json
+import cairosvg
+from PIL import Image
+
+# read the settings file
+with open('settings.json', 'r') as f:
+  settings = json.load(f)
+
+if os.path.exists("./temp") == False:
+  os.mkdir("./temp")
+
+icon_definition_dir = os.fsencode(settings["path_jsonfiles"])
+
+# process all icon definition files
+for file in os.listdir(icon_definition_dir):
+  if os.fsdecode(file).endswith(".json"):
+    filename = os.path.join(icon_definition_dir, file)
+    with open(filename, 'r') as f:
+      curfile = json.load(f)
+    output_path = os.path.join("./output", curfile["path_out_ico"])
+    print(f"process definition file {filename}")
+    print(f"save icons to {output_path}")
+    if os.path.exists(output_path) == False:
+      os.makedirs(output_path)
+
+    # process all icons in the current file
+    for icon in curfile["icon"]:
+      output_file = os.path.join(output_path, icon["file_out"])
+      print(output_file)
+      images=[]
+      sizes=[]
+      # read all inputfiles for this icon
+      for inputfile in icon["file_in"]:
+        imagefile = os.path.join(settings["path_icontheme"], inputfile["src"])
+        tempfile = str(inputfile["size"]) + ".png"
+        tempfile = os.path.join("./temp", tempfile)
+        cairosvg.svg2png(url=imagefile, parent_width = inputfile["size"], parent_height = inputfile["size"], write_to=tempfile)
+        tempsize=(inputfile["size"],inputfile["size"])
+
+        # if we have a specific canvas size, create a new image of its size and there paste the icon centered
+        if "canvas" in inputfile:
+          pad = (inputfile["canvas"]["size"] - inputfile["size"])//2
+          can = Image.new(mode="RGBA",size=(inputfile["canvas"]["size"],inputfile["canvas"]["size"]))
+          can.paste(Image.open(tempfile),(pad,pad))
+          can.save(tempfile)
+          tempsize=(inputfile["canvas"]["size"],inputfile["canvas"]["size"])
+
+        images.append(Image.open(tempfile))
+        sizes.append(tempsize)
+
+      im = Image.new(mode="RGBA", size=(48,48))
+      im.save(output_file, format="ICO", sizes = sizes, append_images = images, bitmap_format="bmp")
+
+    continue
+  else:
+    continue
